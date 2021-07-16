bad8082157af29f5eb2b7d09100ba2aac1b76756
Odin Dutton <odindutton@gmail.com>
2021-07-16 14:49:01 +1000

    Improve example

--- examples/basic.rs
+++ examples/basic.rs
@@ -4,41 +4,41 @@ type ZRGB = inku::Color<inku::ZRGB>;
 const RED: fn(u8, u8, u8, u8) -> (u8, u8, u8, u8) = |_r, g, b, a| (255, g, b, a);
 
 fn main() {
-    eprintln!("Initial color");
     let mut color = RGBA::new(0x01020304);
-    eprintln!("{:?}", color);
+    eprintln!("Initial color\t{:?}", color);
 
-    eprintln!("Lighten 0.5");
     color = color.lighten(0.5);
-    eprintln!("{:?}", color);
+    eprintln!("Lighten 0.5\t{:?}", color);
 
-    eprintln!("Fully increase red channel");
     color = color.map(RED);
-    eprintln!("{:?}", color);
+    eprintln!("Max red channel\t{:?}", color);
 
-    eprintln!("Desaturate");
     for _ in 0..3 {
         color = color.desaturate(0.3);
-        eprintln!("{:?}", color);
+        eprintln!("Desaturate 0.3\t{:?}", color);
     }
 
-    eprintln!("Saturate");
     for _ in 0..3 {
         color = color.saturate(0.3);
-        eprintln!("{:?}", color);
+        eprintln!("Saturate 0.3\t{:?}", color);
     }
 
-    eprintln!("Rotate hue");
     for _ in 0..3 {
         color = color.rotate_hue(45.0);
-        eprintln!("{:?}", color);
+        eprintln!("Rotate hue 45.0\t{:?}", color);
     }
 
-    eprintln!("Darken");
     color = color.darken(0.5);
-    eprintln!("{:?}", color);
+    eprintln!("Darken 0.5\t{:?}", color);
 
-    eprintln!("ZRGB");
-    let color = ZRGB::from(<(u8, u8, u8)>::from(color));
-    eprintln!("{:?}", color);
+    let mut color = ZRGB::from(<(u8, u8, u8)>::from(color));
+    eprintln!("ZRGB\t\t{:?}", color);
+
+    color = color.lighten(0.6);
+    eprintln!("Lighten 0.5\t{:?}", color);
+
+    for _ in 0..3 {
+        color = color.rotate_hue(-90.0);
+        eprintln!("Rotate hue 45.0\t{:?}", color);
+    }
 }
